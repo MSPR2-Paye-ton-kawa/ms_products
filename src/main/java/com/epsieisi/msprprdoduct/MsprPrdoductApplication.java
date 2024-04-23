2f04e2c0296886f5627414024ea3584384c68bfa
Rafenitra <aryfenitraraf@gmail.com>
2024-04-23 09:45:31 +0200

    Initial project of product service

---
+++ src/main/java/com/epsieisi/msprprdoduct/MsprPrdoductApplication.java
@@ -0,0 +1,13 @@
+package com.epsieisi.msprprdoduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprPrdoductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsprPrdoductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epsieisi/msprprdoduct/MsprPrdoductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsieisi.msprprdoduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsprPrdoductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
